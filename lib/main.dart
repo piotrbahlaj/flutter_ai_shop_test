c18f8587a7562d6c2519f2588f5263e46c9efccb
Piotr Bahlaj <piotr.bahlaj@o2.pl>
2025-11-02 05:22:11 +0100

    Setup DI

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ai_shop_test/config/di/di_container.dart';
 import 'package:flutter_ai_shop_test/features/products/presentation/ui/products_screen.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: '.env');
+  setupDependencies();
   runApp(const Main());
 }
 
