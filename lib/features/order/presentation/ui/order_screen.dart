d5e8df2d7e0144bec51ebce9800b7ecb5436dd0e
Piotr Bahlaj <piotr.bahlaj@o2.pl>
2025-11-02 03:58:50 +0100

    Add ui placeholders

---
+++ lib/features/order/presentation/ui/order_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ai_shop_test/features/order/presentation/ui/order_view.dart';
+
+class OrderScreen extends StatelessWidget {
+  const OrderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const OrderView();
+  }
+}
---
+++ lib/features/products/presentation/ui/products_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ai_shop_test/features/products/presentation/ui/products_view.dart';
+
+class ProductsScreen extends StatelessWidget {
+  const ProductsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ProductsView();
+  }
+}
