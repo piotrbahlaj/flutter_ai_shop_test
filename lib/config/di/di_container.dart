7cc5156f9bf725212155bbd3c93d39aee27c1ea4
Piotr Bahlaj <piotr.bahlaj@o2.pl>
2025-11-07 00:28:00 +0100

    Add products repository

--- lib/config/di/di_container.dart
+++ lib/config/di/di_container.dart
@@ -2,7 +2,9 @@ import 'package:dio/dio.dart';
 import 'package:flutter_ai_shop_test/config/environment/environment_config_interface.dart';
 import 'package:flutter_ai_shop_test/core/constants/constants.dart';
 import 'package:flutter_ai_shop_test/core/network/api_client.dart';
+import 'package:flutter_ai_shop_test/features/products/data/repositories/products_repository_impl.dart';
 import 'package:flutter_ai_shop_test/features/products/data/service/products_service.dart';
+import 'package:flutter_ai_shop_test/features/products/domain/repositories/products_repository.dart';
 import 'package:get_it/get_it.dart';
 
 import '../../config/environment/environment_config.dart';
@@ -32,5 +34,9 @@ void setupDependencies() {
   getIt.registerFactory<ProductsService>(
     () =>
         ProductsService(getIt<ApiClient>(instanceName: Constants.dummyJsonDio)),
+  );
+  // Repositories
+  getIt.registerLazySingleton<ProductsRepository>(
+    () => ProductsRepositoryImpl(getIt<ProductsService>()),
   );
 }
--- lib/features/products/data/repositories/products_repository_impl.dart
+++ lib/features/products/data/repositories/products_repository_impl.dart
@@ -1,1 +1,26 @@
-class ProductsRepositoryImpl {}
+import 'package:dio/dio.dart';
+import 'package:flutter_ai_shop_test/core/error/failures.dart';
+import 'package:flutter_ai_shop_test/core/network/error_mapper.dart';
+import 'package:flutter_ai_shop_test/core/utils/result.dart';
+import 'package:flutter_ai_shop_test/features/products/data/models/product_model.dart';
+import 'package:flutter_ai_shop_test/features/products/data/service/products_service.dart';
+import 'package:flutter_ai_shop_test/features/products/domain/repositories/products_repository.dart';
+
+class ProductsRepositoryImpl implements ProductsRepository {
+  final ProductsService _service;
+  ProductsRepositoryImpl(this._service);
+
+  @override
+  Future<Result<List<Product>>> getProducts() async {
+    try {
+      final products = await _service.getProducts();
+      return Result.success(products);
+    } on DioException catch (e) {
+      return Result.failure(
+        mapDioExceptionToFailure(e, source: FailureSource.products),
+      );
+    } on Exception catch (_) {
+      return Result.failure(const UnknownFailure());
+    }
+  }
+}
